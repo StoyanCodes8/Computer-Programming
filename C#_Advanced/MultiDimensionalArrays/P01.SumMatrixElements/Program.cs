2a481875edd0c5382a75bb34faba28e546aeabef
Stoyan Stefanov <stoyanstefanov26@gmail.com>
2025-01-13 15:09:12 +0200

    added a folder

---
+++ C#_Advanced/MultiDimensionalArrays/P01.SumMatrixElements/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P01.SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
