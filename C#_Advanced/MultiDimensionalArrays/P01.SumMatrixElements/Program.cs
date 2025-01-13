b5dd4d0eada890815f086e544710af4a32bd8cb3
Stoyan Stefanov <stoyanstefanov26@gmail.com>
2025-01-13 19:28:44 +0200

    added some exercises

--- C#_Advanced/MultiDimensionalArrays/P01.SumMatrixElements/Program.cs
+++ C#_Advanced/MultiDimensionalArrays/P01.SumMatrixElements/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+      
+
         }
     }
 }
+/* string[] dimensions = Console.ReadLine().Split(", ");
+       int rows = int.Parse(dimensions[0]);
+       int cols = int.Parse(dimensions[1]);
+
+       int[,] matrix = new int[rows, cols];
+       int sum = 0;
+
+       for (int i = 0; i < rows; i++)
+       {
+           string[] rowData = Console.ReadLine().Split(", ");
+           if (rowData.Length != cols)
+           {
+               throw new Exception($"The lengt is incorrect...Should be {cols} characters long!");
+           }
+       }
+       for(int i = 0;i < cols; i++)
+       {
+
+       } */
+
---
+++ C#_Advanced/MultiDimensionalArrays/P02. SumMatrixColumns/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P02._SumMatrixColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dimensions = Console.ReadLine().Split(", ");
+            int rows = int.Parse(dimensions[0]);
+            int cols = int.Parse(dimensions[1]);
+
+            int[][] jaggedMatrix = new int[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                jaggedMatrix[i] = new int[cols];
+                string[] rowData = Console.ReadLine().Split(" ");
+                if (rowData.Length != cols)
+                {
+                    throw new Exception($"The lengt is incorrect...Should be {cols} characters long!");
+                }
+                for (int j = 0; j < cols; j++)
+                {
+                    jaggedMatrix[i][j] = int.Parse(rowData[j]);
+                }
+            }
+        }
+    }
+}
