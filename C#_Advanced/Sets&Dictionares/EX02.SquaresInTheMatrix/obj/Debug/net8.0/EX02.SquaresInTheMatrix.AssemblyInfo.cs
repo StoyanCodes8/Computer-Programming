b5487881ee0e44a69614aba72b8a2af7ba6d13f7
Stoyan Stefanov <stoyanstefanov26@gmail.com>
2025-01-19 13:51:09 +0200

    added EX03...gosh

--- C#_Advanced/Sets&Dictionares/EX02.SquaresInTheMatrix/obj/Debug/net8.0/EX02.SquaresInTheMatrix.AssemblyInfo.cs
+++ C#_Advanced/Sets&Dictionares/EX02.SquaresInTheMatrix/obj/Debug/net8.0/EX02.SquaresInTheMatrix.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EX02.SquaresInTheMatrix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e433c92406418ae7ca3bbb9760f23d1d904deea3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c47c97d0c9ea5d6a40f6a6ba9c51ab7515dca8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EX02.SquaresInTheMatrix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EX02.SquaresInTheMatrix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ C#_Advanced/Sets&Dictionares/EX03.MaximalSum/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace EX03.MaximalSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Normal dimension
+            string[] dimensions = Console.ReadLine().Split(" ");
+            int N = int.Parse(dimensions[0]);
+            int M = int.Parse(dimensions[1]);
+
+            // Outer matrix
+            int[,] matrix = new int[N, M];
+
+            // Cycle
+            for(int i = 0; i < N; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                for(int j = 0; j < M; j++)
+                {
+                    matrix[i, j] = int.Parse(input[j]);
+                }
+            }
+            // ЗАПОЧВАМЕ ОТ N,ЦИКЛИМ ДОКАТО НЕ СТИГНЕМ КРАЯ НА M, j
+            Queue<int> queue = new Queue<int>();
+            Stack<int[]> winningCoordinates = new();
+            int maxSum = int.MinValue;
+            for (int i = 0; i <= N - 3; i++)
+            {
+              for(int j = 0;j <= M - 3; j++)
+                {
+                    int sum = 0;
+                    // обхождане на елементите
+                    for(int k = 0; k < 3; k++) // rows
+                    {
+                        for(int l = 0; l < 3; l++)
+                        {
+                            sum += matrix[i + k, j + l];
+                            // трябва след всяко приключване да съхранявам, защото после пак се пресмята
+                        }
+                    }
+                    queue.Enqueue(sum);
+                    if (sum > maxSum)
+                    {
+                        int desiredStartRow = i;
+                        int desiredStartColl = j;
+                        winningCoordinates.Push(new int[] { desiredStartRow, desiredStartColl });   
+                        maxSum = sum;
+                    }
+                }
+            }
+            // Извеждаме координатите на желаната матрица
+            int[] coordinates = winningCoordinates.Pop();
+            int row = coordinates[0];
+            int col = coordinates[1];
+            // Попълваме желаната матрица
+            int[,] desiredMatrix = new int[3, 3];
+            for(int i = 0;i < 3;i++)
+            {
+                for(int j = 0; j < 3;j++)
+                {
+                    desiredMatrix[i, j] = matrix[row + i, col + j];
+                }
+            }
+
+            // Обхождаме попълнената матрица
+            Console.WriteLine($"Sum = {maxSum}");
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0;j < 3;j++)
+                {
+                    Console.Write(desiredMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+/*1 5 5 2 4
+2 1 4 14 3
+3 7 11 2 8
+4 8 12 16*/
+
+
+
+/*   // Hidden Matrix
+           int sum = 0;
+           Queue<string> queue = new Queue<string>();
+           for(int i = 1; i < outterMatrix.GetLength(0); i++)
+           {
+               for(int j = 1;  j < outterMatrix.GetLength(1)-1; j++)
+               {
+                  sum+=outterMatrix[i, j];
+                  //Console.Write(outterMatrix[i, j]  + " ");
+               }
+           }
+           // Final Result
+           Console.WriteLine($"Sum = {sum}");
+           for (int i = 1; i < outterMatrix.GetLength(0); i++)
+           {
+               for (int j = 1; j < outterMatrix.GetLength(1) - 1; j++)
+               {
+                   Console.Write(outterMatrix[i, j]  + " ");
+               }
+               Console.WriteLine();
+           }
+*/
