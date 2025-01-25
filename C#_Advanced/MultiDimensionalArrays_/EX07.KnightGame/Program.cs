f9e58553ef81da416ec04dfb0cd2fafa89114246
Stoyan Stefanov <stoyanstefanov26@gmail.com>
2025-01-25 10:26:02 +0200

    fixed a folder

---
+++ C#_Advanced/MultiDimensionalArrays_/EX07.KnightGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EX07.KnightGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
