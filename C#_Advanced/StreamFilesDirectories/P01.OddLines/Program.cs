83e3cf376324c72f4b2311d539e59f2d570b8199
Stoyan Stefanov <stoyanstefanov26@gmail.com>
2025-01-30 04:02:25 +0200

    I kinda grasp it now, but not really, so more work!

---
+++ C#_Advanced/StreamFilesDirectories/P01.OddLines/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace P01.OddLines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectRoot = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+            string inputFilePath = Path.Combine(projectRoot, "input.txt");
+            string outputFilePath = Path.Combine(projectRoot, "output.txt");
+            TextOperation(inputFilePath, outputFilePath);
+        }
+        public static void TextOperation(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new(inputFilePath)) 
+            using (StreamWriter writer = new(outputFilePath))
+            {
+                string line;
+                int cycleLength = 0;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if(cycleLength %2 != 0)
+                    {
+                        writer.WriteLine(line);
+                        Console.WriteLine(line);
+                    }
+                    cycleLength++;
+                }
+            }
+        }
+    }
+}
--- C#_Advanced/StreamFilesDirectories/demo/Program.cs
+++ C#_Advanced/StreamFilesDirectories/demo/Program.cs
@@ -5,7 +5,22 @@ namespace demo
     {
         static void Main(string[] args)
         {
-            
+            string inputFilePath = @"..\..\..\Files\\input.txt";
+            string outputFilePath = @"..\..\..\Files\\output.txt";
+            TextOperation(inputFilePath, outputFilePath);
+        }
+        public static void TextOperation(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new(inputFilePath);
+            if(!File.Exists(inputFilePath))
+            {
+                Console.WriteLine("File doesn't exist!");
+            }
+            using (reader)
+            {
+                string input = reader.ReadToEnd();
+                Console.WriteLine(input);
+            }
         }
     }
 }
